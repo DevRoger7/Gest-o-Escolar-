49aeb1d534729c6018230736a9196357c32c49a9
Yudiblimb <yudipro859@gmail.com>
2025-09-21 16:35:41 -0500

    roteamento-dashboard-fix

--- app/main/Views/dashboard/dashboard.php
+++ app/main/Views/dashboard/dashboard.php
@@ -767,7 +767,7 @@ if (!defined('BASE_URL')) {
                 <?php } ?>
                 <?php if (isset($_SESSION['Biblioteca'])) { ?>
                 <li>
-                    <a href="../../subsystems/biblioteca/default.php" class="menu-item flex items-center space-x-3 px-4 py-3 rounded-lg text-gray-700">
+                    <a href="#" class="menu-item flex items-center space-x-3 px-4 py-3 rounded-lg text-gray-700">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                         </svg>
